91ce8f6c241af700a5f394c3e79b00ac5689d6e7
Gsarod <Gsarod@protonmail.com>
2021-07-02 22:22:08 +0200

    V1.1 :- implemented a start of  a terminal user interface

--- Calculatrice/user_interface.py
+++ Calculatrice/user_interface.py
@@ -5,13 +5,26 @@ import Methods
 
 def opeselectmanager() :
 
-    opeselect = int(input("quelle opé voulez vous faire ?  "))
+    #file = open("selopt", "r")  <----- note : Je voudrais utiliser le fichier selopt (Select option), mais je sais pas comment faire, et pour l'instant je vais devoir me résoudre à l'utilisation de la variable opeselect et des elif qu ej'ai mis en place
 
-    if opeselect == 1:
-        Methods.methods.addition()
-    elif opeselect == 2:
-        Methods.methods.soustraction()
-    elif opeselect == 3:
-        Methods.methods.division()
-    elif opeselect == 4:
-        Methods.methods.multipcation()
+    opeselect = int(input("""
+     _________________________________________________________________________
+    |                  Quelle option voulez vous choisir ?                    |
+    |                                                                         |
+    |                                                                         |
+    |-1) Addition                                                             |
+    |-2) Soustraction                                                         |
+    |-3) Multiplication                                                       |
+    |-4)Division                                                              |
+    |_________________________________________________________________________|
+
+    Votre réponse -- >   """)) #sers à donner une meilleure allure au fichier
+
+    if opeselect == 1:                      # \
+        Methods.methods.addition()          # |
+    elif opeselect == 2:                    # |
+        Methods.methods.soustraction()      # | Sert à pouvoir choisir entre les différents opérations
+    elif opeselect == 3:                    # |
+        Methods.methods.division()          # |
+    elif opeselect == 4:                    # |
+        Methods.methods.multipcation()      # /
